c474ff6d2925f416e1d55c118db3ed0eecaedca8
Artem <mokoseev.artem@gmail.com>
2024-08-16 14:31:42 +0300

    tests

--- FightersGame/Fighters.Test/GameManagerTest.cs
+++ FightersGame/Fighters.Test/GameManagerTest.cs
@@ -1,5 +1,8 @@
+using Fighters.Models.Armors;
+using Fighters.Models.Classes;
 using Fighters.Models.Fighters;
 using Fighters.Models.Races;
+using Fighters.Models.Weapons;
 
 namespace Fighters.Test
 {
@@ -7,12 +10,12 @@ namespace Fighters.Test
     public class GameManagerTest
     {
         [Test]
-        public void Game_TwoEqualFighters_FirstFighterWins()
+        public void Game_VampireAndHuman_VampireWins()
         {
             // Arrange 
             var gameManager = new GameManager();
-            var fighterA = new Fighter( "FighterA", new Human() );
-            var fighterB = new Fighter( "FighterB", new Human() );
+            var fighterA = new Fighter( "FighterA", new Vampire(), new Knight() );
+            var fighterB = new Fighter( "FighterB", new Human(), new Knight() );
 
             // Act
             IFighter[] winner = gameManager.Game( fighterA, fighterB );
@@ -22,12 +25,12 @@ namespace Fighters.Test
         }
 
         [Test]
-        public void Game_TwoEqualFighters_SecondFighterDies()
+        public void Game_StrengthOfWeapon_SecondFighterDies()
         {
             // Arrange 
             var gameManager = new GameManager();
-            var fighterA = new Fighter( "FighterA", new Human() );
-            var fighterB = new Fighter( "FighterB", new Human() );
+            var fighterA = new Fighter( "FighterA", new Human(), new Knight(), new Hammer(), new LightArmor() );
+            var fighterB = new Fighter( "FighterB", new Human(), new Knight(), new Spear(), new LightArmor() );
 
             // Act
             gameManager.Game( fighterA, fighterB );
@@ -35,6 +38,24 @@ namespace Fighters.Test
             // Asssert
             Assert.That( fighterA.GetCurrentHealth(), Is.GreaterThan( 0 ) );
             Assert.That( fighterB.GetCurrentHealth(), Is.EqualTo( 0 ) );
+        }
+
+        [Test]
+        public void Game_EqualFighters_IdkWhoDies()
+        {
+            // Arrange 
+            var gameManager = new GameManager();
+            var fighterA = new Fighter( "FighterA", new Human(), new Knight(), new Fists(), new LightArmor() );
+            var fighterB = new Fighter( "FighterB", new Human(), new Knight(), new Fists(), new LightArmor() );
+
+            // Act
+            gameManager.Game( fighterA, fighterB );
+
+            // Asssert
+            Assert.That( fighterA.GetCurrentHealth(), Is.GreaterThan( 0 ) );
+            Assert.That( fighterB.GetCurrentHealth(), Is.EqualTo( 0 ) );
+
+            //this doesnt work because of lack of user activity in test
         }
     }
 }
