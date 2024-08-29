6d31098601ffb9d51c812b5d3f438359c28d8acb
ttemuchin <124429570+ttemuchin@users.noreply.github.com>
2024-08-29 17:28:30 +0300

    Delete FightersGame directory

--- FightersGame/Fighters/Models/Races/Dwarf.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Fighters.Models.Races
-{
-    public class Dwarf : IRace
-    {
-        public int Damage => 3;
-        public int Health => 190;
-        public int Armor => 2;
-    }
-}
--- FightersGame/Fighters/Models/Races/Elf.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Fighters.Models.Races
-{
-    public class Elf : IRace
-    {
-        public int Damage => 5;
-        public int Health => 140;
-        public int Armor => 1;
-    }
-}
