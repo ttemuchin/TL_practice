f65284d503b39c1ce48e1bce901f6b33db9b759a
Artem <mokoseev.artem@gmail.com>
2024-08-29 16:13:29 +0300

    start drawing)) working on Learning process

---
+++ learning-words-app/src/Components/Card.tsx
@@ -0,0 +1,24 @@
+import { Card as CardType } from "../types/Card/Card";
+import React from "react";
+import "./Card.scss";
+
+type CardProps = {
+  card: CardType;
+  showTranslation: boolean;
+};
+
+const Card: React.FC<CardProps> = ({ card, showTranslation }) => {
+  //   const [showTranslation, setShowTranslation] = useState(false);
+
+  //   const flipCard = () => {
+  //     setShowTranslation(!showTranslation);
+  //   }; +++onClick={flipCard}
+
+  return (
+    <div className={`card ${showTranslation ? "flipped" : ""}`}>
+      <div className="card-content">{showTranslation ? card.translation : card.rusWord}</div>
+    </div>
+  );
+};
+
+export default Card;
